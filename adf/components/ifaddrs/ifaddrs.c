e2bdd077dba22e94ef0d88fe9187b0c571e7fb0b
Evan Broder <evan@ebroder.net>
2024-07-23 21:54:49 -0700

    Add a stub implementation of getifaddrs
    This is needed for both kvswebrtc and usersctplib (if we end up using
    that for datachannels). It's definitely not fully POSIX compliant, but
    it should get the job done for our purposes.

---
+++ adf/components/ifaddrs/ifaddrs.c
@@ -0,0 +1,92 @@
+#include "ifaddrs.h"
+#include "errno.h"
+#include "esp_err.h"
+#include "esp_netif.h"
+
+#if defined(CONFIG_LWIP_IPV6)
+#error "IPv6 not supported"
+#endif
+
+static esp_err_t _getifaddrs_cb(void *ctx)
+{
+  esp_err_t err;
+  struct ifaddrs **ifap = (struct ifaddrs **)ctx;
+  esp_netif_t *netif = NULL;
+  while ((netif = esp_netif_next_unsafe(netif)) != NULL)
+  {
+    struct ifaddrs *ifa = calloc(1, sizeof(struct ifaddrs));
+    if (ifa == NULL)
+    {
+      freeifaddrs(*ifap);
+      return ESP_ERR_NO_MEM;
+    }
+
+    ifa->ifa_next = *ifap;
+    *ifap = ifa;
+
+    esp_netif_ip_info_t ip_info;
+    err = esp_netif_get_ip_info(netif, &ip_info);
+    if (err != ESP_OK)
+    {
+      freeifaddrs(*ifap);
+      return err;
+    }
+
+    struct sockaddr_in *addr = calloc(1, sizeof(struct sockaddr_in));
+    if (!addr)
+    {
+      freeifaddrs(*ifap);
+      return ESP_ERR_NO_MEM;
+    }
+    addr->sin_family = AF_INET;
+    addr->sin_addr.s_addr = ip_info.ip.addr;
+    ifa->ifa_addr = (struct sockaddr *)addr;
+
+    ifa->ifa_name = calloc(1, NETIF_NAMESIZE);
+    if (!ifa->ifa_name)
+    {
+      freeifaddrs(*ifap);
+      return ESP_ERR_NO_MEM;
+    }
+
+    err = esp_netif_get_netif_impl_name(netif, ifa->ifa_name);
+    if (err != ESP_OK)
+    {
+      freeifaddrs(*ifap);
+      return err;
+    }
+
+    ifa->ifa_flags = esp_netif_is_netif_up(netif) ? IFF_UP | IFF_RUNNING : 0;
+  }
+
+  return ESP_OK;
+}
+
+int getifaddrs(struct ifaddrs **ifap)
+{
+  esp_err_t err = esp_netif_tcpip_exec(_getifaddrs_cb, ifap);
+  if (err != ESP_OK)
+  {
+    errno = ENOSYS;
+    return -1;
+  }
+  return 0;
+}
+
+void freeifaddrs(struct ifaddrs *ifp)
+{
+  while (ifp)
+  {
+    struct ifaddrs *next = ifp->ifa_next;
+    if (ifp->ifa_addr)
+    {
+      free(ifp->ifa_addr);
+    }
+    if (ifp->ifa_name)
+    {
+      free(ifp->ifa_name);
+    }
+    free(ifp);
+    ifp = next;
+  }
+}
---
+++ adf/components/ifaddrs/include/ifaddrs.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+// These should come from <net/if.h> but lwip doesn't have them
+#define IFF_UP          0x1             /* interface is up */
+#define IFF_BROADCAST   0x2             /* broadcast address valid */
+#define IFF_LOOPBACK    0x8             /* is a loopback net */
+#define IFF_POINTOPOINT 0x10            /* interface is point-to-point link */
+#define IFF_RUNNING     0x40            /* resources allocated */
+
+// This doesn't need to be complete; just good enough for kvswebrtc, which
+// only looks at a handful of fields: ifa_addr, ifa_name, and ifa_flags (and
+// there only IFF_LOOPBACK and IFF_RUNNING, which are always false and true,
+// respectively)
+struct ifaddrs
+{
+  struct ifaddrs *ifa_next;     /* Pointer to next struct */
+  char *ifa_name;               /* Interface name */
+  u_int ifa_flags;              /* Interface flags */
+  struct sockaddr *ifa_addr;    /* Interface address */
+};
+
+int getifaddrs(struct ifaddrs **ifap);
+
+void freeifaddrs(struct ifaddrs *ifp);
