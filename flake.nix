0a691bb6279b14c60d9c961dde4467da114caf0c
Quentin Smith <quentin@mit.edu>
2024-09-15 17:41:48 -0400

    Update pinned ESP-IDF to v5.3.1

--- flake.nix
+++ flake.nix
@@ -2,25 +2,17 @@
   description = "Hunt 2025 Radio";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs-esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev/31ada76b5aa9c5fb7781a327dac74b731ed48521";
+  inputs.nixpkgs-esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
 
   outputs = { self, nixpkgs, flake-utils, nixpkgs-esp-dev }: let
     overlay = (final: prev: {
       radio-esp-idf = (prev.esp-idf-esp32s3.override {
-        rev = "v5.2.2";
-        sha256 = "sha256-I4YxxSGdQT8twkoFx3zmZhyLTSagmeLD2pygVfY/pEk=";
-        toolsToInclude = [
-          "xtensa-esp-elf"
-          "esp32ulp-elf"
-          "openocd-esp32"
-          "xtensa-esp-elf-gdb"
-        ];
         pkg-config = final.pkg-config-unwrapped;
       }).overrideAttrs (old: {
         patches = (old.patches or []) ++ [
           (final.fetchpatch {
-            url = "https://github.com/espressif/esp-adf/raw/master/idf_patches/idf_v5.2_freertos.patch";
-            hash = "sha256-PlpG9qE0jC0+ltbqdIPrg89vUdMx7C946xX36cjdxDc=";
+            url = "https://github.com/espressif/esp-adf/raw/master/idf_patches/idf_v5.3_freertos.patch";
+            hash = "sha256-8EfnrOv4aJrlJJnoRzxx7tzXbMXpxFx53cAGZFxEm6g=";
           })
         ];
       });
@@ -28,8 +20,8 @@
         owner = "espressif";
         repo = "esp-adf";
         fetchSubmodules = true;
-        rev = "0593164bf7e64c268dad56e332d6f07f0e9c37ab";
-        hash = "sha256-oVJHV5jd4lnRUanyQTpmqLgtA2627uSxpyIDwVgq1lk=";
+        rev = "ff7f39dcf0a0da87671ff4ab422eb258be429049";
+        hash = "sha256-C8kQ2AaAeQtOjt9BiGraXAiOlgtaVsXLnIIu9lZA88M=";
       };
     });
   in
