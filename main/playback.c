a7406fb016f00c0691a0b9b57d3e07603b54494c
Evan Broder <evan@ebroder.net>
2024-10-15 12:57:00 -0700

    Tweak playback API to allow starting and stopping

--- main/playback.c
+++ main/playback.c
@@ -14,138 +14,225 @@
 #include "opus_decoder.h"
 #include "wav_decoder.h"
 
-esp_err_t playback_file(const char *path, bool duck_others) {
-  int fd = -1;
-  audio_pipeline_handle_t pipeline = NULL;
-  audio_event_iface_handle_t evt = NULL;
-  mixer_channel_t channel = NULL;
-  ringbuf_handle_t output = NULL;
+struct playback_handle {
+  char *path;
+  bool duck_others;
+  int fd;
+  audio_pipeline_handle_t pipeline;
+  audio_element_handle_t decoder;
+  audio_event_iface_handle_t evt;
+  ringbuf_handle_t output;
+  bool tuned;
+  audio_element_info_t info;
+  mixer_channel_t channel;
+};
+
+esp_err_t playback_file(playback_cfg_t *cfg, playback_handle_t *return_handle) {
+  ESP_RETURN_ON_FALSE(return_handle, ESP_ERR_INVALID_ARG, RADIO_TAG,
+                      "Invalid handle");
 
   esp_err_t ret = ESP_OK;
 
-  fd = file_cache_open_file(path);
-  ESP_RETURN_ON_FALSE(fd >= 0, ESP_FAIL, RADIO_TAG, "Failed to open file: %d",
-                      fd);
+  playback_handle_t handle = calloc(1, sizeof(struct playback_handle));
+  ESP_RETURN_ON_FALSE(handle, ESP_ERR_NO_MEM, RADIO_TAG,
+                      "Failed to allocate memory for playback handle");
+  handle->path = strdup(cfg->path);
+  handle->duck_others = cfg->duck_others;
+  handle->tuned = cfg->tuned;
+
+  handle->fd = file_cache_open_file(handle->path);
+  ESP_RETURN_ON_FALSE(handle->fd >= 0, ESP_FAIL, RADIO_TAG,
+                      "Failed to open file: %d", handle->fd);
 
   audio_pipeline_cfg_t pipeline_cfg = {
       .rb_size = 2048,
   };
-  pipeline = audio_pipeline_init(&pipeline_cfg);
-  ESP_RETURN_ON_FALSE(pipeline, ESP_FAIL, RADIO_TAG,
+  handle->pipeline = audio_pipeline_init(&pipeline_cfg);
+  ESP_RETURN_ON_FALSE(handle->pipeline, ESP_FAIL, RADIO_TAG,
                       "Failed to create audio pipeline");
 
-  output = rb_create(960 * 2, 1);
+  handle->output = rb_create(960 * 2, 1);
+  ESP_GOTO_ON_ERROR(handle->output == NULL, cleanup, RADIO_TAG,
+                    "Failed to create output ringbuf");
 
   file_stream_cfg_t file_cfg = FILE_STREAM_CFG_DEFAULT();
-  file_cfg.fd = fd;
+  file_cfg.fd = handle->fd;
   audio_element_handle_t file_stream = file_stream_init(&file_cfg);
   ESP_GOTO_ON_FALSE(file_stream, ESP_FAIL, cleanup, RADIO_TAG,
                     "Failed to create file stream");
-  ESP_GOTO_ON_ERROR(audio_pipeline_register(pipeline, file_stream, "file"),
-                    cleanup, RADIO_TAG,
-                    "Failed to register file stream to pipeline");
+  ESP_GOTO_ON_ERROR(
+      audio_pipeline_register(handle->pipeline, file_stream, "file"), cleanup,
+      RADIO_TAG, "Failed to register file stream to pipeline");
 
-  audio_element_handle_t decoder = NULL;
-  const char *ext = strrchr(path, '.');
+  const char *ext = strrchr(handle->path, '.');
   if (ext != NULL && strcasecmp(ext, ".opus") == 0) {
     opus_decoder_cfg_t opus_cfg = DEFAULT_OPUS_DECODER_CONFIG();
     opus_cfg.task_core = 0;
-    decoder = decoder_opus_init(&opus_cfg);
-    ESP_GOTO_ON_FALSE(decoder, ESP_FAIL, cleanup, RADIO_TAG,
+    handle->decoder = decoder_opus_init(&opus_cfg);
+    ESP_GOTO_ON_FALSE(handle->decoder, ESP_FAIL, cleanup, RADIO_TAG,
                       "Failed to create Opus decoder");
   } else {
     wav_decoder_cfg_t wav_cfg = DEFAULT_WAV_DECODER_CONFIG();
-    decoder = wav_decoder_init(&wav_cfg);
-    ESP_GOTO_ON_FALSE(decoder, ESP_FAIL, cleanup, RADIO_TAG,
+    handle->decoder = wav_decoder_init(&wav_cfg);
+    ESP_GOTO_ON_FALSE(handle->decoder, ESP_FAIL, cleanup, RADIO_TAG,
                       "Failed to create WAV decoder");
   }
-  ESP_GOTO_ON_ERROR(audio_pipeline_register(pipeline, decoder, "decoder"),
-                    cleanup, RADIO_TAG,
-                    "Failed to register decoder to pipeline");
-  ESP_GOTO_ON_ERROR(audio_element_set_output_ringbuf(decoder, output), cleanup,
-                    RADIO_TAG, "Failed to set output ringbuf for decoder");
-
   ESP_GOTO_ON_ERROR(
-      audio_pipeline_link(pipeline, (const char *[]){"file", "decoder"}, 2),
-      cleanup, RADIO_TAG, "Failed to link file and decoder to pipeline");
+      audio_pipeline_register(handle->pipeline, handle->decoder, "decoder"),
+      cleanup, RADIO_TAG, "Failed to register decoder to pipeline");
+  ESP_GOTO_ON_ERROR(
+      audio_element_set_output_ringbuf(handle->decoder, handle->output),
+      cleanup, RADIO_TAG, "Failed to set output ringbuf for decoder");
+
+  ESP_GOTO_ON_ERROR(audio_pipeline_link(handle->pipeline,
+                                        (const char *[]){"file", "decoder"}, 2),
+                    cleanup, RADIO_TAG,
+                    "Failed to link file and decoder to pipeline");
 
   audio_event_iface_cfg_t evt_cfg = AUDIO_EVENT_IFACE_DEFAULT_CFG();
-  evt = audio_event_iface_init(&evt_cfg);
-  ESP_GOTO_ON_FALSE(evt, ESP_FAIL, cleanup, RADIO_TAG,
+  handle->evt = audio_event_iface_init(&evt_cfg);
+  ESP_GOTO_ON_FALSE(handle->evt, ESP_FAIL, cleanup, RADIO_TAG,
                     "Failed to create event interface");
-  ESP_GOTO_ON_ERROR(audio_pipeline_set_listener(pipeline, evt), cleanup,
-                    RADIO_TAG, "Failed to set listener for pipeline");
+  ESP_GOTO_ON_ERROR(audio_pipeline_set_listener(handle->pipeline, handle->evt),
+                    cleanup, RADIO_TAG, "Failed to set listener for pipeline");
 
-  ESP_GOTO_ON_ERROR(audio_pipeline_run(pipeline), cleanup, RADIO_TAG,
+  ESP_GOTO_ON_ERROR(audio_pipeline_run(handle->pipeline), cleanup, RADIO_TAG,
                     "Failed to start audio pipeline");
 
-  // TODO: we need some way for the caller to interrupt playback
+cleanup:
+  if (ret != ESP_OK) {
+    playback_free(handle);
+    handle = NULL;
+  }
+
+  *return_handle = handle;
+  return ret;
+}
+
+esp_err_t playback_wait_for_completion(playback_handle_t handle) {
+  ESP_RETURN_ON_FALSE(handle, ESP_ERR_INVALID_ARG, RADIO_TAG,
+                      "Invalid playback handle");
+
   while (1) {
     audio_event_iface_msg_t msg;
-    esp_err_t ret = audio_event_iface_listen(evt, &msg, portMAX_DELAY);
+    esp_err_t ret = audio_event_iface_listen(handle->evt, &msg, portMAX_DELAY);
     if (ret != ESP_OK) {
       ESP_LOGW(RADIO_TAG, "Error listening to event during playback: %d (%s)",
                ret, esp_err_to_name(ret));
       continue;
     }
 
-    // We only care about the decoder element
-    if (msg.source_type != AUDIO_ELEMENT_TYPE_ELEMENT ||
-        msg.source != (void *)decoder) {
-      continue;
+    if (msg.source_type == AUDIO_ELEMENT_TYPE_PLAYER &&
+        msg.source == (void *)handle) {
+      if (msg.cmd == AEL_MSG_CMD_PAUSE) {
+        handle->tuned = false;
+      } else if (msg.cmd == AEL_MSG_CMD_RESUME) {
+        handle->tuned = true;
+      }
     }
 
-    // Don't plug into mixer until we get a REPORT_MUSIC_INFO event, since that
-    // tells us the audio format
-    if (msg.cmd == AEL_MSG_CMD_REPORT_MUSIC_INFO) {
-      audio_element_info_t info;
-      ESP_GOTO_ON_ERROR(audio_element_getinfo(decoder, &info), cleanup,
-                        RADIO_TAG, "Failed to get decoder info");
-      ESP_LOGD(RADIO_TAG, "Playback audio format (%s): %d Hz, %d bits, %s",
-               path, info.sample_rates, info.bits,
-               info.channels == 1 ? "mono" : "stereo");
-
-      if (!channel) {
-        ESP_GOTO_ON_ERROR(mixer_play_audio((mixer_read_callback_t)rb_read,
-                                           output, info.sample_rates, info.bits,
-                                           info.channels, duck_others,
-                                           &channel),
-                          cleanup, RADIO_TAG, "Failed to add audio to mixer");
+    if (msg.source_type == AUDIO_ELEMENT_TYPE_ELEMENT &&
+        msg.source == (void *)handle->decoder) {
+      // Don't plug into mixer until we get a REPORT_MUSIC_INFO event, since
+      // that tells us the audio format
+      if (msg.cmd == AEL_MSG_CMD_REPORT_MUSIC_INFO) {
+        ESP_RETURN_ON_ERROR(
+            audio_element_getinfo(handle->decoder, &handle->info), RADIO_TAG,
+            "Failed to get decoder info");
+        ESP_LOGD(RADIO_TAG, "Playback audio format (%s): %d Hz, %d bits, %s",
+                 handle->path, handle->info.sample_rates, handle->info.bits,
+                 handle->info.channels == 1 ? "mono" : "stereo");
       }
 
-      continue;
+      if (msg.cmd == AEL_MSG_CMD_REPORT_STATUS &&
+          ((int)msg.data == AEL_STATUS_STATE_FINISHED ||
+           (int)msg.data == AEL_STATUS_STATE_STOPPED)) {
+        break;
+      }
     }
 
-    if (msg.cmd == AEL_MSG_CMD_REPORT_STATUS &&
-        ((int)msg.data == AEL_STATUS_STATE_FINISHED ||
-         (int)msg.data == AEL_STATUS_STATE_STOPPED)) {
-      break;
+    bool currently_playing = handle->channel;
+    bool should_play = handle->tuned && handle->info.sample_rates;
+    if (currently_playing && !should_play) {
+      mixer_stop_audio(handle->channel);
+      handle->channel = NULL;
+    } else if (!currently_playing && should_play) {
+      ESP_RETURN_ON_ERROR(
+          mixer_play_audio((mixer_read_callback_t)rb_read, handle->output,
+                           handle->info.sample_rates, handle->info.bits,
+                           handle->info.channels, handle->duck_others,
+                           &handle->channel),
+          RADIO_TAG, "Failed to add audio to mixer");
     }
   }
 
-cleanup:
-  if (channel) {
-    mixer_stop_audio(channel);
+  if (handle->channel) {
+    mixer_stop_audio(handle->channel);
+    handle->channel = NULL;
   }
 
-  if (pipeline) {
-    audio_pipeline_stop(pipeline);
-    audio_pipeline_wait_for_stop(pipeline);
-    // This will deinit all registered elements, too
-    audio_pipeline_deinit(pipeline);
+  return ESP_OK;
+}
+
+esp_err_t playback_detune(playback_handle_t handle) {
+  ESP_RETURN_ON_FALSE(handle, ESP_ERR_INVALID_ARG, RADIO_TAG,
+                      "Invalid playback handle");
+  ESP_RETURN_ON_ERROR(
+      audio_event_iface_sendout(handle->evt,
+                                &(audio_event_iface_msg_t){
+                                    .source = handle,
+                                    .source_type = AUDIO_ELEMENT_TYPE_PLAYER,
+                                    .cmd = AEL_MSG_CMD_PAUSE,
+                                }),
+      RADIO_TAG, "Failed to detune playback");
+  return ESP_OK;
+}
+
+esp_err_t playback_entune(playback_handle_t handle) {
+  ESP_RETURN_ON_FALSE(handle, ESP_ERR_INVALID_ARG, RADIO_TAG,
+                      "Invalid playback handle");
+
+  ESP_RETURN_ON_ERROR(
+      audio_event_iface_sendout(handle->evt,
+                                &(audio_event_iface_msg_t){
+                                    .source = handle,
+                                    .source_type = AUDIO_ELEMENT_TYPE_PLAYER,
+                                    .cmd = AEL_MSG_CMD_RESUME,
+                                }),
+      RADIO_TAG, "Failed to entune playback");
+  return ESP_OK;
+}
+
+void playback_free(playback_handle_t handle) {
+  if (!handle) {
+    return;
   }
 
-  if (output) {
-    rb_destroy(output);
+  if (handle->channel) {
+    mixer_stop_audio(handle->channel);
   }
 
-  if (evt) {
-    audio_event_iface_destroy(evt);
+  if (handle->pipeline) {
+    audio_pipeline_stop(handle->pipeline);
+    audio_pipeline_wait_for_stop(handle->pipeline);
+    audio_pipeline_deinit(handle->pipeline);
   }
 
-  if (fd >= 0) {
-    close(fd);
+  if (handle->output) {
+    rb_destroy(handle->output);
   }
 
-  return ret;
+  if (handle->evt) {
+    audio_event_iface_destroy(handle->evt);
+  }
+
+  if (handle->fd >= 0) {
+    close(handle->fd);
+  }
+
+  if (handle->path) {
+    free(handle->path);
+  }
+
+  free(handle);
 }
--- main/playback.h
+++ main/playback.h
@@ -8,7 +8,19 @@
 extern "C" {
 #endif
 
-esp_err_t playback_file(const char *path, bool duck_others);
+typedef struct {
+  const char *path;
+  bool duck_others;
+  bool tuned;
+} playback_cfg_t;
+
+typedef struct playback_handle *playback_handle_t;
+
+esp_err_t playback_file(playback_cfg_t *cfg, playback_handle_t *handle);
+esp_err_t playback_wait_for_completion(playback_handle_t handle);
+esp_err_t playback_detune(playback_handle_t handle);
+esp_err_t playback_entune(playback_handle_t handle);
+void playback_free(playback_handle_t handle);
 
 #ifdef __cplusplus
 }
