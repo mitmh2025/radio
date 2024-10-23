f6840ddc38029bdf4e227502f61f9760367bd31f
Evan Broder <evan@ebroder.net>
2024-10-23 13:03:51 -0700

    Decrease gain on static even more

--- main/static.c
+++ main/static.c
@@ -8,8 +8,8 @@ int static_read_audio(void *ctx, char *data, int len,
                       TickType_t ticks_to_wait) {
   int16_t *samples = (int16_t *)data;
   for (int i = 0; i < len / 2; i++) {
-    // Hard code a gain of 0.25 for now
-    samples[i] = (int16_t)(rand() & 0xFFFF) >> 2;
+    // Hard code a gain of 1/8 for now
+    samples[i] = (int16_t)(rand() & 0xFFFF) >> 3;
   }
   return len;
 }
