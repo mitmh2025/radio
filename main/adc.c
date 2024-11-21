d99aefdc5aca532395d233d7f665eaa416b82645
Evan Broder <evan@ebroder.net>
2024-11-21 14:03:44 -0800

    Rework ADC task
    Always start/stop the ADC from adc_task, instead of whoever is
    subscribing/unsubscribing. (This avoids a bug where the ADC crashes if
    it's stopped from a different task than it's started from).
    Additionally, add a watchdog that fires if we've received no new ADC
    data for over 1.5 seconds, to try and catch whatever ADC lockup we've
    been experiencing.

--- main/adc.c
+++ main/adc.c
@@ -10,6 +10,7 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
+#include "freertos/timers.h"
 
 static SemaphoreHandle_t adc_mutex = NULL;
 static EXT_RAM_BSS_ATTR struct adc_config {
@@ -20,9 +21,14 @@ static EXT_RAM_BSS_ATTR struct adc_config {
 } adc_configs[SOC_ADC_MAX_CHANNEL_NUM] = {};
 static adc_continuous_handle_t adc_handle = NULL;
 static bool adc_running = false;
+static TaskHandle_t adc_task_handle = NULL;
+
+static TimerHandle_t adc_watchdog = NULL;
 
 static atomic_int_fast64_t last_adc_conv_done = 0;
 static atomic_int_fast64_t last_adc_read = 0;
+
+#define NOTIFY_ADC_RESTART BIT(0)
 
 static void telemetry_generator() {
   things_send_telemetry_int("adc_conv_done", atomic_load(&last_adc_conv_done));
@@ -35,84 +41,10 @@ static bool IRAM_ATTR adc_conv_done_cb(adc_continuous_handle_t handle,
                                        void *user_data) {
   atomic_store(&last_adc_conv_done, esp_timer_get_time());
 
-  TaskHandle_t adc_task_handle = (TaskHandle_t)user_data;
   BaseType_t must_yield = pdFALSE;
-  vTaskNotifyGiveFromISR(adc_task_handle, &must_yield);
+  xTaskNotifyFromISR(adc_task_handle, 0, eNoAction, &must_yield);
 
   return must_yield == pdTRUE;
-}
-
-static void adc_task(void *context) {
-  while (true) {
-    ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(1000));
-
-    // Continue reading as long as there is data to read
-    while (true) {
-      uint8_t result[SOC_ADC_DIGI_DATA_BYTES_PER_CONV *
-                     SOC_ADC_MAX_CHANNEL_NUM] = {};
-      uint32_t out_length;
-      esp_err_t ret = adc_continuous_read(adc_handle, result, sizeof(result),
-                                          &out_length, 100);
-      if (ret == ESP_ERR_TIMEOUT          // No pending data
-          || ret == ESP_ERR_INVALID_STATE // The driver is already stopped
-      ) {
-        // Wait until the interrupt wakes us up
-        break;
-      } else if (ret != ESP_OK) {
-        ESP_LOGE(RADIO_TAG, "adc_continuous_read failed: %d", ret);
-        break;
-      }
-
-      atomic_store(&last_adc_read, esp_timer_get_time());
-
-      xSemaphoreTake(adc_mutex, portMAX_DELAY);
-      for (int i = 0; i < out_length; i += SOC_ADC_DIGI_RESULT_BYTES) {
-        adc_digi_output_data_t *data = (adc_digi_output_data_t *)&result[i];
-        struct adc_config *config = &adc_configs[data->type2.channel];
-        if (config->callback) {
-          config->callback(config->user_data, data);
-        } else {
-          ESP_LOGW(RADIO_TAG, "No callback for channel %d",
-                   data->type2.channel);
-        }
-      }
-      xSemaphoreGive(adc_mutex);
-    }
-  }
-}
-
-esp_err_t adc_init() {
-  adc_mutex = xSemaphoreCreateMutex();
-  if (adc_mutex == NULL) {
-    return ESP_ERR_NO_MEM;
-  }
-
-  adc_continuous_handle_cfg_t cfg = {
-      .conv_frame_size =
-          SOC_ADC_DIGI_DATA_BYTES_PER_CONV * SOC_ADC_MAX_CHANNEL_NUM,
-      .max_store_buf_size =
-          SOC_ADC_DIGI_DATA_BYTES_PER_CONV * SOC_ADC_MAX_CHANNEL_NUM * 8,
-      .flags = {.flush_pool = true},
-  };
-  ESP_RETURN_ON_ERROR(adc_continuous_new_handle(&cfg, &adc_handle), RADIO_TAG,
-                      "adc_init failed");
-
-  TaskHandle_t adc_task_handle = NULL;
-  ESP_RETURN_ON_FALSE(pdPASS == xTaskCreatePinnedToCore(adc_task, "adc", 4096,
-                                                        NULL, 18,
-                                                        &adc_task_handle, 0),
-                      ESP_FAIL, RADIO_TAG, "Failed to create adc task");
-
-  adc_continuous_evt_cbs_t cbs = {
-      .on_conv_done = adc_conv_done_cb,
-  };
-  ESP_RETURN_ON_ERROR(adc_continuous_register_event_callbacks(adc_handle, &cbs,
-                                                              adc_task_handle),
-                      RADIO_TAG, "adc_init failed");
-
-  things_register_telemetry_generator(telemetry_generator, "adc", NULL);
-
-  return ESP_OK;
 }
 
 static esp_err_t adc_config_and_start() {
@@ -150,6 +82,108 @@ static esp_err_t adc_config_and_start() {
                       "Failed to start continuous ADC");
 
   adc_running = true;
+
+  xTimerStart(adc_watchdog, portMAX_DELAY);
+
+  return ESP_OK;
+}
+
+static void adc_task(void *context) {
+  xSemaphoreTake(adc_mutex, portMAX_DELAY);
+  ESP_ERROR_CHECK_WITHOUT_ABORT(adc_config_and_start());
+  xSemaphoreGive(adc_mutex);
+
+  while (true) {
+    uint32_t notification;
+    xTaskNotifyWait(0, ULONG_MAX, &notification, pdMS_TO_TICKS(1000));
+    if (notification & NOTIFY_ADC_RESTART) {
+      xSemaphoreTake(adc_mutex, portMAX_DELAY);
+      if (adc_running) {
+        adc_continuous_stop(adc_handle);
+        adc_running = false;
+        xTimerStop(adc_watchdog, portMAX_DELAY);
+      }
+      ESP_ERROR_CHECK_WITHOUT_ABORT(adc_config_and_start());
+      xSemaphoreGive(adc_mutex);
+    }
+
+    // Continue reading as long as there is data to read
+    while (true) {
+      uint8_t result[SOC_ADC_DIGI_DATA_BYTES_PER_CONV *
+                     SOC_ADC_MAX_CHANNEL_NUM] = {};
+      uint32_t out_length;
+      esp_err_t ret = adc_continuous_read(adc_handle, result, sizeof(result),
+                                          &out_length, 0);
+      if (ret == ESP_ERR_TIMEOUT          // No pending data
+          || ret == ESP_ERR_INVALID_STATE // The driver is already stopped
+      ) {
+        // Wait until the interrupt wakes us up
+        break;
+      } else if (ret != ESP_OK) {
+        ESP_LOGE(RADIO_TAG, "adc_continuous_read failed: %d", ret);
+        break;
+      }
+
+      atomic_store(&last_adc_read, esp_timer_get_time());
+
+      xSemaphoreTake(adc_mutex, portMAX_DELAY);
+      for (int i = 0; i < out_length; i += SOC_ADC_DIGI_RESULT_BYTES) {
+        adc_digi_output_data_t *data = (adc_digi_output_data_t *)&result[i];
+        struct adc_config *config = &adc_configs[data->type2.channel];
+        if (config->callback) {
+          config->callback(config->user_data, data);
+        } else {
+          ESP_LOGW(RADIO_TAG, "No callback for channel %d",
+                   data->type2.channel);
+        }
+      }
+      xSemaphoreGive(adc_mutex);
+    }
+  }
+}
+
+static void adc_watchdog_cb(TimerHandle_t timer) {
+  if (esp_timer_get_time() - atomic_load(&last_adc_read) > 1500000) {
+    ESP_LOGW(RADIO_TAG, "ADC watchdog triggered; restarting ADC");
+    xTaskNotify(adc_task_handle, NOTIFY_ADC_RESTART, eSetBits);
+  }
+}
+
+esp_err_t adc_init() {
+  adc_mutex = xSemaphoreCreateMutex();
+  if (adc_mutex == NULL) {
+    return ESP_ERR_NO_MEM;
+  }
+
+  adc_continuous_handle_cfg_t cfg = {
+      .conv_frame_size =
+          SOC_ADC_DIGI_DATA_BYTES_PER_CONV * SOC_ADC_MAX_CHANNEL_NUM,
+      .max_store_buf_size =
+          SOC_ADC_DIGI_DATA_BYTES_PER_CONV * SOC_ADC_MAX_CHANNEL_NUM * 8,
+      .flags = {.flush_pool = true},
+  };
+  ESP_RETURN_ON_ERROR(adc_continuous_new_handle(&cfg, &adc_handle), RADIO_TAG,
+                      "adc_init failed");
+
+  ESP_RETURN_ON_FALSE(pdPASS == xTaskCreatePinnedToCore(adc_task, "adc", 4096,
+                                                        NULL, 18,
+                                                        &adc_task_handle, 0),
+                      ESP_FAIL, RADIO_TAG, "Failed to create adc task");
+
+  adc_continuous_evt_cbs_t cbs = {
+      .on_conv_done = adc_conv_done_cb,
+  };
+  ESP_RETURN_ON_ERROR(
+      adc_continuous_register_event_callbacks(adc_handle, &cbs, NULL),
+      RADIO_TAG, "adc_init failed");
+
+  adc_watchdog = xTimerCreate("adc_watchdog", pdMS_TO_TICKS(1000), pdTRUE, 0,
+                              adc_watchdog_cb);
+  ESP_RETURN_ON_FALSE(adc_watchdog, ESP_ERR_NO_MEM, RADIO_TAG,
+                      "Failed to create ADC watchdog");
+
+  things_register_telemetry_generator(telemetry_generator, "adc", NULL);
+
   return ESP_OK;
 }
 
@@ -175,17 +209,12 @@ esp_err_t adc_subscribe(adc_digi_pattern_config_t *config,
                     "adc_subscribe channel %d already subscribed",
                     config->channel);
 
-  if (adc_running) {
-    adc_continuous_stop(adc_handle);
-    adc_running = false;
-  }
-
   adc_configs[config->channel].populated = true;
   adc_configs[config->channel].config = *config;
   adc_configs[config->channel].callback = callback;
   adc_configs[config->channel].user_data = user_data;
 
-  ret = adc_config_and_start();
+  xTaskNotify(adc_task_handle, NOTIFY_ADC_RESTART, eSetBits);
 
 cleanup:
   xSemaphoreGive(adc_mutex);
@@ -206,15 +235,10 @@ esp_err_t adc_unsubscribe(uint8_t channel) {
                     cleanup, RADIO_TAG,
                     "adc_unsubscribe channel %d not subscribed", channel);
 
-  if (adc_running) {
-    adc_continuous_stop(adc_handle);
-    adc_running = false;
-  }
-
   adc_configs[channel].populated = false;
   adc_configs[channel].callback = NULL;
 
-  ret = adc_config_and_start();
+  xTaskNotify(adc_task_handle, NOTIFY_ADC_RESTART, eSetBits);
 
 cleanup:
   xSemaphoreGive(adc_mutex);
