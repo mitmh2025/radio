477bd22117f6e184cfe503f51e3ab21a8edf60bd
Evan Broder <evan@ebroder.net>
2024-12-30 13:41:06 -0800

    Rework battery to no longer need dedicated task

--- main/battery.c
+++ main/battery.c
@@ -11,6 +11,8 @@
 #include "esp_check.h"
 #include "esp_log.h"
 #include "esp_random.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
 
 #define DISCHARGE_LOW_BATTERY_THRESHOLD_MV                                     \
   (2333) // 3.5V is roughly 20% of an 18650 * our 2/3 voltage divider
@@ -26,67 +28,85 @@ typedef enum {
   BATTERY_DISCHARGING,
 } battery_status_t;
 
+typedef enum {
+  BATTERY_INDICATOR_CHARGED,
+  BATTERY_INDICATOR_CHARGING,
+  BATTERY_INDICATOR_LOW,
+  BATTERY_INDICATOR_DISCHARGING,
+} battery_indicator_t;
+
 struct color {
   uint32_t red;
   uint32_t green;
   uint32_t blue;
 };
 
-static struct color charged_color = {
-    .red = 10,
-    .green = 64,
-    .blue = 0,
-};
-
-static struct color charging_color = {
-    .red = 64,
-    .green = 25,
-    .blue = 0,
-};
-
-static struct color low_color = {
-    .red = 64,
-    .green = 0,
-    .blue = 0,
-};
-
-static struct color discharging_color = {
-    .red = 0,
-    .green = 0,
-    .blue = 0,
+static struct color colors[] = {
+    [BATTERY_INDICATOR_CHARGED] =
+        {
+            .red = 10,
+            .green = 64,
+            .blue = 0,
+        },
+    [BATTERY_INDICATOR_CHARGING] =
+        {
+            .red = 64,
+            .green = 25,
+            .blue = 0,
+        },
+    [BATTERY_INDICATOR_LOW] =
+        {
+            .red = 64,
+            .green = 0,
+            .blue = 0,
+        },
+    [BATTERY_INDICATOR_DISCHARGING] =
+        {
+            .red = 0,
+            .green = 0,
+            .blue = 0,
+        },
 };
 
 static uint32_t battery_average = 0;
+
+static SemaphoreHandle_t status_mutex = NULL;
 static bool battery_low = false;
 static battery_status_t battery_status = BATTERY_DISCHARGING;
+static battery_indicator_t battery_indicator = BATTERY_INDICATOR_DISCHARGING;
 
 static adc_cali_handle_t battery_adc_cali;
 static i2c_master_dev_handle_t battery_i2c_device;
-static StaticTask_t battery_monitor_task_buffer;
-static EXT_RAM_BSS_ATTR StackType_t battery_monitor_task_stack[4096];
-static TaskHandle_t battery_monitor_task_handle = NULL;
+static esp_timer_handle_t battery_timer = NULL;
 static size_t battery_telemetry_index = 0;
 
-static void battery_update_led() {
+static void battery_update_state() {
+  battery_indicator_t new_indicator;
   if (battery_low) {
-    led_set_pixel(0, low_color.red, low_color.green, low_color.blue);
+    new_indicator = BATTERY_INDICATOR_LOW;
   } else {
     switch (battery_status) {
     case BATTERY_CHARGED:
-      led_set_pixel(0, charged_color.red, charged_color.green,
-                    charged_color.blue);
+      new_indicator = BATTERY_INDICATOR_CHARGED;
       break;
     case BATTERY_CHARGING:
-      led_set_pixel(0, charging_color.red, charging_color.green,
-                    charging_color.blue);
+      new_indicator = BATTERY_INDICATOR_CHARGING;
       break;
     case BATTERY_DISCHARGING:
-      led_set_pixel(0, discharging_color.red, discharging_color.green,
-                    discharging_color.blue);
+      new_indicator = BATTERY_INDICATOR_DISCHARGING;
       break;
     default:
+      new_indicator = BATTERY_INDICATOR_DISCHARGING;
       break;
     }
+  }
+
+  if (new_indicator != battery_indicator) {
+    battery_indicator = new_indicator;
+    led_set_pixel(0, colors[battery_indicator].red,
+                  colors[battery_indicator].green,
+                  colors[battery_indicator].blue);
+    things_force_telemetry(battery_telemetry_index);
   }
 }
 
@@ -100,19 +120,23 @@ static void battery_adc_callback(void *user_data,
   battery_average =
       battery_average - (battery_average >> 3) + (result->type2.data >> 3);
 
+  bool new_battery_low = battery_low;
+  // Check if the battery crossed the low threshold
   if (battery_status == BATTERY_DISCHARGING &&
-      battery_average < discharge_low_battery_threshold_raw && !battery_low) {
-    battery_low = true;
-    if (battery_monitor_task_handle != NULL) {
-      xTaskNotify(battery_monitor_task_handle, 0, eNoAction);
-    }
+      battery_average < discharge_low_battery_threshold_raw &&
+      !new_battery_low) {
+    new_battery_low = true;
   } else if (battery_status != BATTERY_DISCHARGING &&
              battery_average > charge_low_battery_threshold_raw &&
-             battery_low) {
-    battery_low = false;
-    if (battery_monitor_task_handle != NULL) {
-      xTaskNotify(battery_monitor_task_handle, 0, eNoAction);
-    }
+             new_battery_low) {
+    new_battery_low = false;
+  }
+
+  if (new_battery_low != battery_low) {
+    xSemaphoreTake(status_mutex, portMAX_DELAY);
+    battery_low = new_battery_low;
+    battery_update_state();
+    xSemaphoreGive(status_mutex);
   }
 }
 
@@ -145,46 +169,46 @@ static void battery_telemetry_generator() {
   things_send_telemetry_string("battery_status", status);
 }
 
-static void battery_monitor(void *context) {
-  while (true) {
-    uint8_t addr = IP5306_REG_READ0;
-    ip5306_reg_read0_t reg0_value;
-    ip5306_reg_read1_t reg1_value;
-    BOARD_I2C_MUTEX_LOCK();
-    esp_err_t err = i2c_master_transmit_receive(battery_i2c_device, &addr, 1,
-                                                &reg0_value.raw, 1, -1);
-    if (err != ESP_OK) {
-      ESP_LOGE(RADIO_TAG, "Failed to read IP5306 register 0");
-      goto cleanup;
-    }
-    addr = IP5306_REG_READ1;
-    err = i2c_master_transmit_receive(battery_i2c_device, &addr, 1,
-                                      &reg1_value.raw, 1, -1);
-    if (err != ESP_OK) {
-      ESP_LOGE(RADIO_TAG, "Failed to read IP5306 register 1");
-      goto cleanup;
-    }
+static void battery_timer_cb(void *context) {
+  uint8_t addr = IP5306_REG_READ0;
+  ip5306_reg_read0_t reg0_value;
+  ip5306_reg_read1_t reg1_value;
+  BOARD_I2C_MUTEX_LOCK();
+  esp_err_t err = i2c_master_transmit_receive(battery_i2c_device, &addr, 1,
+                                              &reg0_value.raw, 1, -1);
+  if (err != ESP_OK) {
+    ESP_LOGE(RADIO_TAG, "Failed to read IP5306 register 0");
+    goto cleanup;
+  }
+  addr = IP5306_REG_READ1;
+  err = i2c_master_transmit_receive(battery_i2c_device, &addr, 1,
+                                    &reg1_value.raw, 1, -1);
+  if (err != ESP_OK) {
+    ESP_LOGE(RADIO_TAG, "Failed to read IP5306 register 1");
+    goto cleanup;
+  }
 
-  cleanup:
-    BOARD_I2C_MUTEX_UNLOCK();
-    if (err == ESP_OK) {
-      battery_status_t new_status =
-          !reg0_value.parsed.CHARGE_ENABLE ? BATTERY_DISCHARGING
-          : reg1_value.parsed.CHARGING     ? BATTERY_CHARGING
-                                           : BATTERY_CHARGED;
-      if (new_status != battery_status) {
-        battery_status = new_status;
-        things_force_telemetry(battery_telemetry_index);
-      }
+cleanup:
+  BOARD_I2C_MUTEX_UNLOCK();
+  if (err == ESP_OK) {
+    battery_status_t new_status =
+        !reg0_value.parsed.CHARGE_ENABLE ? BATTERY_DISCHARGING
+        : reg1_value.parsed.CHARGING     ? BATTERY_CHARGING
+                                         : BATTERY_CHARGED;
+    if (new_status != battery_status) {
+      xSemaphoreTake(status_mutex, portMAX_DELAY);
+      battery_status = new_status;
+      battery_update_state();
+      xSemaphoreGive(status_mutex);
     }
-
-    battery_update_led();
-    xTaskNotifyWait(0, ULONG_MAX, NULL,
-                    pdMS_TO_TICKS(1000 + esp_random() % 100));
   }
 }
 
 esp_err_t battery_init() {
+  status_mutex = xSemaphoreCreateMutex();
+  ESP_RETURN_ON_FALSE(status_mutex != NULL, ESP_ERR_NO_MEM, RADIO_TAG,
+                      "Failed to create status mutex");
+
   adc_digi_pattern_config_t adc_cfg = {
       .atten = ADC_ATTEN_DB_12,
       .channel = BATTERY_ADC_CHANNEL,
@@ -247,12 +271,19 @@ esp_err_t battery_init() {
   BOARD_I2C_MUTEX_UNLOCK();
   ESP_RETURN_ON_ERROR(err, RADIO_TAG, "i2c_master_bus_add_device failed");
 
-  battery_monitor_task_handle = xTaskCreateStatic(
-      battery_monitor, "battery_monitor",
-      sizeof(battery_monitor_task_stack) / sizeof(StackType_t), NULL, 5,
-      battery_monitor_task_stack, &battery_monitor_task_buffer);
-  ESP_RETURN_ON_FALSE(battery_monitor_task_handle, ESP_FAIL, RADIO_TAG,
-                      "Failed to create battery monitor task");
+  err = esp_timer_create(
+      &(esp_timer_create_args_t){
+          .callback = battery_timer_cb,
+          .arg = NULL,
+          .dispatch_method = ESP_TIMER_TASK,
+          .name = "battery_timer",
+      },
+      &battery_timer);
+  ESP_RETURN_ON_ERROR(err, RADIO_TAG, "Failed to create battery timer");
+  battery_timer_cb(NULL);
+  err = esp_timer_start_periodic(battery_timer, 500 * 1000);
+  ESP_RETURN_ON_ERROR(err, RADIO_TAG, "Failed to start battery timer");
+
   things_register_telemetry_generator(battery_telemetry_generator, "battery",
                                       &battery_telemetry_index);
 
